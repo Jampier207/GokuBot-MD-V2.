ebe4acfcdff428b5e91ed703aca2dcb84e21a60e
𖤍𝑱𝑨𝑴𝑷𝑰𝑬𝑹207𖤍 <142342932+Jampier207@users.noreply.github.com>
2026-03-09 09:11:51 -0500

    V2.0.0

--- commands/socket/setbotname.js
+++ commands/socket/setbotname.js
@@ -2,14 +2,18 @@ export default {
   command: ['setbotname', 'setname'],
   category: 'socket',
   run: async (client, m, args, usedPrefix) => {
+
     const command = m?.body?.split(' ')[0]?.slice(usedPrefix.length) || 'setbotname'
     if (!m?.sender) return
+
     const idBot = client.user.id.split(':')[0] + '@s.whatsapp.net'
-    const config = global.db.data.settings[idBot]
+    const config = global.db.data.settings[idBot] ||= {}
+
     const isOwner2 = [idBot, ...global.owner.map(n => n + '@s.whatsapp.net')].includes(m.sender)
-    if (!isOwner2 && m.sender !== owner) return m.reply('✦ Acceso denegado. Solo el propietario puede usar este comando.')
+    if (!isOwner2) return m.reply('✦ Acceso denegado. Solo el propietario puede usar este comando.')
 
     const value = args.join(' ').trim()
+
     if (!value) return m.reply(
       `✦ Debes escribir un nombre corto y un nombre largo válido.\n` +
       `> Ejemplo: *${usedPrefix + command}* Goku / GokuBot-MD`
@@ -17,6 +21,7 @@ export default {
 
     const formatted = value.replace(/\s*\/\s*/g, '/')
     let [short, long] = formatted.includes('/') ? formatted.split('/') : [value, value]
+
     if (!short || !long) return m.reply('✦ Usa el formato: Nombre Corto / Nombre Largo')
     if (/\s/.test(short)) return m.reply('✦ El nombre corto no puede contener espacios.')
 

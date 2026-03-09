93bf64f0de2f81ad50431608a56718f1ac366cf0
𖤍𝑱𝑨𝑴𝑷𝑰𝑬𝑹207𖤍 <142342932+Jampier207@users.noreply.github.com>
2026-03-08 21:40:32 -0500

    V2.0.0

---
+++ commands/grupo/bot.js
@@ -0,0 +1,57 @@
+export default {
+  command: ['bot'],
+  category: 'grupo',
+  isAdmin: true,
+  run: async (client, m, args) => {
+    const chat = global.db.data.chats[m.chat]
+    const estado = chat.isBanned ?? false
+    const botName = global.db.data.settings[client.user.id.split(':')[0] + "@s.whatsapp.net"].namebot
+
+    if (args[0] === 'off') {
+      if (estado)
+        return m.reply(
+`────────────────────
+🌱 El *Bot* ya estaba *desactivado* en este grupo.
+────────────────────`
+        )
+
+      chat.isBanned = true
+
+      return m.reply(
+`────────────────────
+🌱 Has *Desactivado* a *${botName}* en este grupo.
+────────────────────`
+      )
+    }
+
+    if (args[0] === 'on') {
+      if (!estado)
+        return m.reply(
+`────────────────────
+🌱 *${botName}* ya estaba *activado* en este grupo.
+────────────────────`
+        )
+
+      chat.isBanned = false
+
+      return m.reply(
+`────────────────────
+🌱 Has *Activado* a *${botName}* en este grupo.
+────────────────────`
+      )
+    }
+
+    return m.reply(
+`════════════════════
+🌱 Estado de ${botName}
+
+🌱 Actual › ${estado ? 'Desactivado' : 'Activado'}
+
+🌱 Puede cambiarse con:
+
+• bot on
+• bot off
+════════════════════`
+    )
+  },
+};

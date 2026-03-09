022700a715202914ed1a84bbc59b3ee63069ee1b
𖤍𝑱𝑨𝑴𝑷𝑰𝑬𝑹207𖤍 <142342932+Jampier207@users.noreply.github.com>
2026-03-08 22:14:09 -0500

    V2.0.0

---
+++ commands/owner/exec2.js
@@ -0,0 +1,25 @@
+import cp from 'child_process'
+import { promisify } from 'util'
+
+const exec = promisify(cp.exec)
+
+export default {
+  command: ['r'],
+  isOwner: true,
+  run: async (client, m, text) => {
+    if (!m?.sender) return
+
+    if (!text) return m.reply('[ ❗ ] Debes escribir un comando para ejecutar')
+
+    let o
+    try {
+      o = await exec(text.trim())
+    } catch (e) {
+      o = e
+    } finally {
+      const { stdout, stderr } = o
+      if (stdout && stdout.trim()) await m.reply(stdout)
+      if (stderr && stderr.trim()) await m.reply(stderr)
+    }
+  }
+}

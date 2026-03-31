192489d4de2b54be6515d5455664c5dc033b8c11
𖤍𝑱𝑨𝑴𝑷𝑰𝑬𝑹207𖤍 <142342932+Jampier207@users.noreply.github.com>
2026-03-31 13:57:48 -0500

    Actualizar setbotowner.js

--- commands/socket/setbotowner.js
+++ commands/socket/setbotowner.js
@@ -3,8 +3,7 @@ import { resolveLidToRealJid } from "../../lib/utils.js"
 export default {
   command: ['setbotowner'],
   category: 'socket',
-  run: async (client, m, args, command, usedPrefix) => {
-    if (!m?.sender) return
+  run: async (client, m, args, command, text, prefix) => {
     const idBot = client.user.id.split(':')[0] + '@s.whatsapp.net'
     const config = global.db.data.settings[idBot]
     const isOwner2 = [idBot, ...global.owner.map((number) => number + '@s.whatsapp.net')].includes(m.sender)
@@ -14,7 +13,7 @@ export default {
     const who = await resolveLidToRealJid(who2, client, m.chat);
     const menti = client.user.id.split(':')[0] + "@s.whatsapp.net"
     if (!who2) {
-     return client.reply(m.chat, `✦ Debes mencionar al nuevo dueño del bot.\n> Ejemplo: *${usedPrefix + command}* @${menti.split('@')[0]}*`, m, { mentions: [menti] })
+     return client.reply(m.chat, `❖ Debes mencionar al nuevo dueño del bot.\n> Ejemplo: *${prefix + command} @${menti.split('@')[0]}*`, m, { mentions: [menti] })
     }
 
 const anteriorOwner = config.owner
@@ -23,8 +22,8 @@ const esCambio = anteriorOwner && anteriorOwner.endsWith('@s.whatsapp.net')
 config.owner = who
 
 const mensaje = esCambio
-  ? `✦ El dueño del bot ha sido cambiado de @${anteriorOwner.split('@')[0]} a @${who.split('@')[0]}!`
-  : `✦ El nuevo dueño de *${config.namebot2}* es @${who.split('@')[0]}!`
+  ? `❖ El dueño del bot ha sido cambiado de @${anteriorOwner.split('@')[0]} a @${who.split('@')[0]}!`
+  : `❖ El nuevo dueño de *${config.namebot2}* es @${who.split('@')[0]}!`
 
 return m.reply(mensaje, {
   mentions: esCambio ? [anteriorOwner, who] : [who]

6a6a76c31b4bad29dfe2e38bd8f4a39f296c9653
𖤍𝑱𝑨𝑴𝑷𝑰𝑬𝑹207𖤍 <142342932+Jampier207@users.noreply.github.com>
2026-03-31 14:11:14 -0500

    Actualizar setpfp.js

--- commands/socket/setpfp.js
+++ commands/socket/setpfp.js
@@ -15,17 +15,16 @@ export default {
   command: ['setimage', 'setpfp'],
   category: 'socket',
   run: async (client, m, args) => {
-    if (!m?.sender) return
     const idBot = client.user.id.split(':')[0] + '@s.whatsapp.net'
     const config = global.db.data.settings[idBot]
     const isOwner2 = [idBot, ...global.owner.map((number) => number + '@s.whatsapp.net')].includes(m.sender)
     if (!isOwner2 && m.sender !== owner) return m.reply(mess.socket)
     const q = m.quoted || m
     const mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (!/image/g.test(mime)) return m.reply('✐ Debes enviar o citar una imagen para cambiar la foto de perfil del bot.')
+    if (!/image/g.test(mime)) return m.reply('❖ Debes enviar o citar una imagen para cambiar la foto de perfil del bot.')
 
     const media = await q.download()
-    if (!media) return m.reply('※ No se pudo descargar la imagen.')
+    if (!media) return m.reply('❖ No se pudo descargar la imagen.')
 
     const jid = client.user.id.split(':')[0] + '@s.whatsapp.net'
 
@@ -50,6 +49,6 @@ export default {
       await client.updateProfilePicture(jid, media)
     }
 
-    return m.reply(`> ✦ Se ha actualizado la foto de perfil de *${config.namebot}*!`)
+    return m.reply(`✎ Se ha actualizado la foto de perfil de *${config.namebot}*!`)
   },
 };
